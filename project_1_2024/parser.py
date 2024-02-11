d120d1542bafad5e6eb2336fa0d28d9efa6381af
Alisher1806 <amazirinov@gmail.com>
2024-02-11 11:34:23 +0100

    need to test

--- project_1_2024/parser.py
+++ project_1_2024/parser.py
@@ -1,21 +1,6 @@
 from xml.dom import minidom
 import sys, re
 
-"""
-
-prog = xml.createElement('program')
-xml.appendChild(prog)
-prog.setAttribute('language', 'IPPcode24')
-
-progChild = xml.createElement('child')
-progChild.setAttribute('name', 'test_test')
-progCtest = xml.createTextNode("child_test")
-progChild.appendChild(progCtest)
-prog.appendChild(progChild)
-
-
-xml_str = xml.toprettyxml(indent='\t', encoding="UTF-8").decode("utf-8")"""
-
 
 xml = minidom.Document()
 order = 0
@@ -125,6 +110,33 @@ def checkArgumentTwo(instrName, argName):
     else:
         sys.exit(23)
         
+def checkArgumentThree(argName):
+    global regVar
+    global regInt
+    global regString
+    global regBool
+    global regNil
+    
+    if(re.match(regVar, argName)):
+        var = argName.split('@')
+        return var
+    elif(re.match(regInt, argName)):
+        var = argName.split('@')
+        return var
+    elif(re.match(regString, argName)):
+        var = argName.split('@')
+        return var
+    elif(re.match(regBool, argName)):
+        var = argName.split('@')
+        return var
+    elif(re.match(regNil, argName)):
+        var = argName.split('@')
+        return var
+    else:
+        sys.exit(23)
+        
+        
+        
 def specialSymbol(argName):  
     for elem in argName:
         if('&' in elem):
@@ -134,14 +146,168 @@ def specialSymbol(argName):
         elif('>' in elem):
             elem = elem.replace('>', "&gt;")
         else:
-            continue;
-    return argName    
+            continue
+    return argName
 
 def createXML_for3(line, order, num):
-    pass
+    global xml
+    global prog
+    
+    instr = xml.createElement("instruction")
+    prog.appendChild(instr)
+    instr.setAttribute("order", str(order))
+    instr.setAttribute("opcode", line[0])
+    
+    # FIRST ARGUMENT
+
+    
+    argument1 = checkArgumentOne(line[0], line[1])
+    if(argument1[0] == 'string'):
+        argument1 = specialSymbol(argument1)
+        
+    arg1 = xml.createElement("arg1")
+    arg1_text = xml.createTextNode(line[1])
+    arg1.appendChild(arg1_text)
+    instr.appendChild(arg1)
+    
+    if(num == 1):
+        argument1[0] = "var"
+        arg1.setAttribute("type", argument1[0])
+    elif(num == 0):
+        arg1.setAttribute("type", argument1)
+    
+    # SECOND ARGUMENT
+
+    argument2 = checkArgumentTwo(line[0], line[2])
+    if(argument2[0] == 'string'):
+        argument2 = specialSymbol(argument2)
+    if(argument2[0] == "GF" or argument2[0] == "LF" or argument2[0] == "TF"):
+            newArg2 = '@'.join(map(str, argument2))
+            argument2[0] = "var"
+            arg2 = xml.createElement("arg2")
+            arg2_text = xml.createTextNode(newArg2)
+            arg2.appendChild(arg2_text)
+            instr.appendChild(arg2)
+    elif(argument2[0] == "int" or argument2[0] == "string" or argument2[0] == "bool"):
+        if(argument2[0] == "string"):
+            merge = ""
+            for i in argument2:
+                if(merge == "" and i != "string"):
+                    merge = merge + i
+                elif(i == "string"):
+                    continue
+                else:
+                    merge = merge + '@' + i
+            arg2 = xml.createElement("arg2")
+            arg2_text = xml.createTextNode(merge)
+        else:
+            arg2 = xml.createElement("arg2")
+            arg2_text = xml.createTextNode(argument2[1])  
+    arg2.appendChild(arg2_text)
+    instr.appendChild(arg2)
+    arg2.setAttribute("type", argument2[0])
+    
+    # THIRD ARGUMENT
+    
+    argument3 = checkArgumentThree(line[3])
+    if(argument3[0] == 'string'):
+        argument3 = specialSymbol(argument3)
+    
+    if(argument3[0] == "GF" or argument3[0] == "LF" or argument3[0] == "TF"):
+            newArg3 = '@'.join(map(str, argument3))
+            argument3[0] = "var"
+            arg3 = xml.createElement("arg3")
+            arg3_text = xml.createTextNode(newArg3)
+            arg3.appendChild(arg3_text)
+            instr.appendChild(arg3)
+    elif(argument3[0] == "int" or argument3[0] == "string" or argument3[0] == "bool"):
+        if(argument3[0] == "string"):
+            merge = ""
+            for i in argument3:
+                if(merge == "" and i != "string"):
+                    merge = merge + i
+                elif(i == "string"):
+                    continue
+                else:
+                    merge = merge + '@' + i
+            arg3 = xml.createElement("arg3")
+            arg3_text = xml.createTextNode(merge)
+        else:
+            arg3 = xml.createElement("arg3")
+            arg3_text = xml.createTextNode(argument3[1])  
+    arg3.appendChild(arg3_text)
+    instr.appendChild(arg3)
+    arg3.setAttribute("type", argument3[0])
 
 def createXML_for2(line, order, num):
-    pass
+    global xml
+    global prog
+    
+    instr = xml.createElement("instruction")
+    prog.appendChild(instr)
+    instr.setAttribute("order", str(order))
+    instr.setAttribute("opcode", line[0])
+    
+    argument1 = checkArgumentOne(line[0], line[1])
+    
+    if(argument1[0] == 'string'):
+        argument1 = specialSymbol(argument1)
+    
+    arg1 = xml.createElement("arg1")
+    arg1_text = xml.createTextNode(line[1])
+    arg1.appendChild(arg1_text)
+    instr.appendChild(arg1)
+    argument1[0] = "var"
+    arg1.setAttribute("type", argument1[0])
+    
+    argument2 = checkArgumentTwo(line[0], line[2])
+    
+    if(argument2[0] == 'string'):
+        argument2 = specialSymbol(argument2)
+
+    if(num == 1):
+        if(argument2[0] == "GF" or argument2[0] == "LF" or argument2[0] == "TF"):
+            newArg2 = '@'.join(map(str, argument2))
+            node = "var"
+            arg2 = xml.createElement("arg2")
+            arg2_text = xml.createTextNode(newArg2)
+            arg2.appendChild(arg2_text)
+            instr.appendChild(arg2)
+            arg2.setAttribute("type", node)
+        elif(argument2[0] == "int" or argument2[0] == "string" or argument2[0] == "bool"):
+            if(argument2[0] == "string"):
+                node = "string"
+                merge = ""
+                for i in argument2:
+                    if(merge == "" and i != "string"):
+                        merge = merge + i
+                    elif(i == "string"):
+                        continue
+                    else:
+                        merge = merge + '@' + i
+                arg2 = xml.createElement("arg2")
+                arg2_text = xml.createTextNode(merge)
+            else:
+                arg2 = xml.createElement("arg2")
+                arg2_text = xml.createTextNode(argument2[1])
+                
+            arg2.appendChild(arg2_text)
+            instr.appendChild(arg2)
+            arg2.setAttribute("type", argument2[0])
+        elif(argument2[0] == "nil"):
+            arg2 = xml.createElement("arg2")
+            arg2_text = xml.createTextNode(argument2[1])
+            arg2.appendChild(arg2_text)
+            instr.appendChild(arg2)
+            arg2.setAttribute("type", argument2[0])
+        else:
+            arg2 = xml.createElement("arg2")
+            arg2_text = xml.createTextNode(line[2])
+            arg2.appendChild(arg2_text)
+            instr.appendChild(arg2)
+            arg2.setAttribute("type", argument2)
+
+    
 
 def createXML_for1(line, order, num):
     global xml
@@ -152,6 +318,8 @@ def createXML_for1(line, order, num):
     instr.setAttribute("order", str(order))
     instr.setAttribute("opcode", line[0])
     
+    #print(line[0])
+    
     argument = checkArgumentOne(line[0], line[1])
     if(argument[0] == 'string'):
         argument = specialSymbol(argument)
@@ -159,13 +327,13 @@ def createXML_for1(line, order, num):
     if(num == 1):
         if(argument[0] == "GF" or argument[0] == "LF" or argument[0] == "TF"):
             newArg1 = '@'.join(map(str, argument))
-            node = "var"
+            argument[0] = "var"
             arg1 = xml.createElement("arg1")
             arg1_text = xml.createTextNode(newArg1)
             arg1.appendChild(arg1_text)
         else:
             if(argument[0] == "string"):
-                node = "string"
+                #node = "string"
                 merge = ""
                 for i in argument:
                     if(merge == "" and i != "string"):
@@ -176,7 +344,7 @@ def createXML_for1(line, order, num):
                         merge = merge + '@' + i
                 arg1 = xml.createElement("arg1")
                 arg1_text = xml.createTextNode(merge)
-                print(argument)
+                #print(argument)
                 arg1.appendChild(arg1_text)
             else:
                 arg1 = xml.createElement("arg1")
@@ -184,9 +352,9 @@ def createXML_for1(line, order, num):
                 arg1.appendChild(arg1_text)
                        
         instr.appendChild(arg1)
-        arg1.setAttribute("type", node)
+        arg1.setAttribute("type", argument[0])
     else:
-        arg1 = xml.createElement("arg1");
+        arg1 = xml.createElement("arg1")
         arg1_text = xml.createTextNode(line[1])
         arg1.appendChild(arg1_text)
         instr.appendChild(arg1)
@@ -230,14 +398,14 @@ def checkline(line):
             
     elif(line[0] in zeroArgs and args == 0):
         order += 1
-        print("this")
+        #print("this")
         createXML_for0(line, order)
         
     elif(line[0] not in instructions):
-        print("err 22")
+        #print("err 22")
         sys.exit(22)
     else:
-        print("err 23")
+        #print("err 23")
         sys.exit(23)
     
     
@@ -275,7 +443,7 @@ def scanner():
             sys.exit(21)
         
         if(line_arr[0] == '.IPPCODE24'):
-            continue;
+            continue
         
         #print(line_arr)
         checkline(line_arr)
